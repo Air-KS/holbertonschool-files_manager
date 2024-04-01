96302ad51066f3cd69a2121436731762f3c98486
Air-KS <kevinrogeret@gmail.com>
2024-04-01 19:45:10 +0200

    Appcontroller

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,29 +1,22 @@
-const { checkRedis, checkDatabase } = require('../utils');
-const User = require('../models/User');
-const File = require('../models/File');
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
-const AppController = {
-  getStatus: async (req, res) => {
-    try {
-      const redisStatus = await checkRedis();
-      const dbStatus = await checkDatabase();
-
-      res.status(200).json({ redis: redisStatus, db: dbStatus });
-    } catch (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  },
-
-  getStats: async (req, res) => {
-    try {
-      const userCount = await User.countDocuments();
-      const fileCount = await File.countDocuments();
-
-      res.status(200).json({ users: userCount, files: fileCount });
-    } catch (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
+class AppController {
+  static getStatus(req, res) {
+    const status = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    res.status(200).send(status);
   }
-};
 
-module.exports = AppController;
+  static async getStats(req, res) {
+    const stats = {
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
+    };
+    res.status(200).send(stats);
+  }
+}
+
+export default AppController;
