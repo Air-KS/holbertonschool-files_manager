9289980373cb301481c4297c6df0076e6689a91a
mxnc <thibaultmaxence44@gmail.com>
2024-04-03 11:59:13 +0200

    fixing for ESLint

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,14 +1,20 @@
-import dbClient from '../utils/db';
 import sha1 from 'sha1';
+import dbClient from '../utils/db';
 
 class UsersController {
   static async postNew(req, res) {
     const { email, password } = req.body;
 
-    if (!email) return res.status(400).send({ error: 'Missing email' });
-    if (!password) return res.status(400).send({ error: 'Missing password' });
+    if (!email) {
+      return res.status(400).send({ error: 'Missing email' });
+    }
+    if (!password) {
+      return res.status(400).send({ error: 'Missing password' });
+    }
     const emailExists = await dbClient.users.findOne({ email });
-    if (emailExists) return res.status(400).send({ error: 'Already exist' });
+    if (emailExists) {
+      return res.status(400).send({ error: 'Already exist' });
+    }
 
     const user = { email, password: sha1(password) };
     const result = await dbClient.db.collection('users').insertOne(user);
